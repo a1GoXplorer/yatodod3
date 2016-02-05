834a7e393ed929f86bb37e9c7e41d4ad53e77d16
Mark Daniels <mark_daniels1982@hotmail.com>
2016-02-05 09:57:16 -0800

    fix todos

--- app.js
+++ app.js
@@ -12,40 +12,29 @@
   }));
   app.set('view engine', 'ejs');
 
-  var things = [{item: "jar of sauce", owner: "the feds"}, {item: "red paper", owner: "hopless masses"}, {item: "free will", owner: "the french"}, {item: "mashed potato", owner: "lonely person"}];
+  var todos = [{todo: "Make todo app"}, {todo: "Floss"}, {todo: "Pay people"}, {todo: "Take out trash"}];
+
+  app.get('/todos', function(req, res) {
+    res.render('todos/index.ejs', {todosList: todos})
+  });
+
+  app.post('/todos', function(req, res) {
+    console.log(req.body);
+    todos.push(req.body);
+    res.redirect('/todos');
+  });
+
+  app.get('/todos/new', function(req, res) {
+    res.render('todos/new.ejs')
+  });
   
 
   app.get('/', function(req, res) {
-    res.render('search.ejs', {name: "MarkMarkMarkMarkMark (aka Mark x 5)"});
-    console.log(req.body);
-  });
-
-  app.get('/things', function(req, res) {
-    res.render('things/index.ejs', {thingsList: things})
-  });
-
-  app.post('/things', function(req, res) {
-    console.log(req.body);
-    things.push(req.body);
-    res.redirect('/things');
-  });
-
-  app.get('/things/new', function(req, res) {
-    res.render('things/new.ejs')
-  });
-
-  app.get('/search', function(req, res) {
-    var searchTerm = req.body.movieTitle;
-    var url = "http://www.ombdapi.com/?s=" + searchTerm;
-    request(url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var obj = JSON.parse(body);
-        res.render('results.ejs', {movieList: obj.Search});
-      }
-    });
+    res.render('index.ejs');
   });
 
 
+  //listen...do you want to know a secret?
   app.listen(3000, function() {
     console.log("Lights are on.")
   });
